2d47f83f579f004956b1d419a1f772c5a2f568cf
Kevin Coyle <kevin.coyle@drutek.co.uk>
2017-02-20 02:23:14 +0000

    Adjusted src and destinations

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/')
-   .sass('src/app.scss', 'dist/');
+mix.js('src/js/app.js', 'dist/')
+  .sass('src/scss/app.scss', 'dist/');
 
 // Full API
 // mix.js(src, output);
